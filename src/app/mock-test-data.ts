f755b40642201982bf2248d224c947f02a10a226
Ria Nicole Carmin <akimalunar@gmail.com>
2017-02-26 19:49:36 -0800

    Angular CLI version fix

--- src/app/mock-test-data.ts
+++ src/app/mock-test-data.ts
@@ -1,7 +1,7 @@
 import { Question } from './test/question/question';
 
-export const TESTDATA: Question[] = [  
-  {text: "Question 1?", options: ["one", "two", "three"], answer:"two"},
-  {text: "Question 2?", options: ["one", "two", "three"], answer:"two"},
-  {text: "Question 3?", options: ["one", "two", "three"], answer:"two"}
+export const TESTDATA: Question[] = [
+  {text: 'Question 1?', options: ['one', 'two', 'three'], answer:'two'},
+  {text: 'Question 2?', options: ['one', 'two', 'three'], answer:'two'},
+  {text: 'Question 3?', options: ['one', 'two', 'three'], answer:'two'}
 ]
