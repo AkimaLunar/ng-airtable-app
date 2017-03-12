0ad668e7e506111ec74ec4d5cc705fb9ae3c6f89
Ria Carmin <akimalunar@gmail.com>
2017-03-12 14:21:53 -0700

    data in JSON format

--- src/app/mock-test-data.ts
+++
@@ -1,8 +0,0 @@
-import { Question } from './test/question/question';
-
-export const TESTDATA: Question[] = [
-  {text: 'Who is the cutest?', options: ['Ivy', 'David', 'Ria'], answer: 'Ivy'},
-  {text: 'Who is the smelliest?', options: ['Ivy', 'David', 'Ria'], answer: 'David'},
-  {text: 'Who forgot to wash the dishes?', options: ['Ivy', 'David', 'Ria'], answer: 'Ria'},
-  {text: 'Who is always grumpy?', options: ['Ivy', 'David', 'Ria'], answer: 'Ria'},
-];
