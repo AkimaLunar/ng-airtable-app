c3f69dd3f2fa61abd388217714ac51c387dfd5b2
Ria Nicole Carmin <akimalunar@gmail.com>
2017-03-13 17:57:45 -0700

    adding Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HeaderModule } from './header/header.module';
 import { QuizModule } from './quiz/quiz.module';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule,
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule
   ],
   declarations: [HeaderComponent],
   exports: [
